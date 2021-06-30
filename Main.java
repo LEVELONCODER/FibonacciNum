879465fbd4b7bf6b38732fbf7d8ece025f99d805
LevSheinJava-development <81438214+LEVELONCODER@users.noreply.github.com>
2021-06-30 20:22:09 +0500

    Update Main.java

--- Main.java
+++ Main.java
@@ -1,9 +1,17 @@
-import org.fibonacci.num.Fibonacci; // If error with package create it, Если промблема с библиотекой, создайте её
-
+import org.fibonacci.num.Fibonacci;
+/*
+ * If error with package create it
+ *  Если ошибка с библиотекой, создайте её
+ * If error with class do it not public
+ * Если ошибка с классом сделайте его не публичным
+*/
 public class Main {
 
     public static void main(String[] args) {
         Fibonacci fibonacci = new Fibonacci();
-        //Подключение библиотеки org.fibonacci.num с файлом Fibonacci.java
+        /*
+         * Подключение библиотеки org.fibonacci.num с файлом Fibonacci.java
+         * Include library org.fibonacci.num with file Fibonacci.Java
+        */
     }
 }
