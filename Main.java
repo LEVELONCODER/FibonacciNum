a32e0727031e0bc0b6fc3c4e14b054a817662921
LevSheinJava-development <81438214+LEVELONCODER@users.noreply.github.com>
2021-06-30 20:08:56 +0500

    Create Main.java

---
+++ Main.java
@@ -0,0 +1,9 @@
+import org.fibonacci.num.Fibonacci;
+
+public class Main {
+
+    public static void main(String[] args) {
+        Fibonacci fibonacci = new Fibonacci();
+        //Подключение библиотеки org.fibonacci.num с файлом Fibonacci.java
+    }
+}
